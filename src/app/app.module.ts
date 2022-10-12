03ba69aaa5974ae263499b1e4311cc2ec5e7d568
Akshayjosephtomy <akshayjt007@gmail.com>
2022-10-12 21:35:03 +0530

    todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TodoComponent } from './todo/todo.component';
+
+
+const link:Routes=[
+  {
+    path:"",component:TodoComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +22,8 @@ import { TodoComponent } from './todo/todo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(link)
   ],
   providers: [],
   bootstrap: [AppComponent]
